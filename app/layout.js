b573f53c50625e4807296c3fda1f6f4ece3b83a7
iamohits <76800512+iamohits@users.noreply.github.com>
2023-08-09 12:29:47 +0800

    robots improvement to index

--- app/layout.js
+++ app/layout.js
@@ -33,14 +33,9 @@ export const metadata = {
   robots: {
     index: true,
     follow: true,
-    nocache: true,
     googleBot: {
       index: true,
       follow: true,
-      noimageindex: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
     },
   },
   icons: {
--- app/robots.js
+++ app/robots.js
@@ -3,7 +3,6 @@ const robots = () => {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/private/",
     },
     sitemap: "https://iamohit.com/sitemap.xml",
   };
