5004b05638f6e88d50829d1f20ca6b056bf37da4
iamohits <76800512+iamohits@users.noreply.github.com>
2023-08-09 12:42:05 +0800

    Robots X-Robots-Tag Indexed

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    const headers = [];
+    headers.push({
+      headers: [
+        {
+          key: "X-Robots-Tag",
+          value: "index",
+        },
+      ],
+      source: "/",
+    });
+    return headers;
+  },
+};
 
 module.exports = nextConfig;
