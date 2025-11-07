e460329125816328a714248764cfee56be0c0f04
mohit5783 <mohit5783@gmail.com>
2025-11-07 11:38:49 +0800

    corrected build error, sitemap + jsonlds

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -2,7 +2,6 @@ import { getPostBySlug, getAllPostSlugs } from "@/lib/blog";
 import { notFound } from "next/navigation";
 import BlogPostClient from "./BlogPostClient";
 
-// Add this to the metadata for each blog post
 export async function generateMetadata({ params }) {
   const resolvedParams = await params;
   const post = getPostBySlug(resolvedParams.slug);
@@ -85,31 +84,6 @@ export async function generateStaticParams() {
   return slugs.map((item) => ({
     slug: item.slug,
   }));
-}
-
-export async function generateMetadata({ params }) {
-  const resolvedParams = await params;
-  const post = getPostBySlug(resolvedParams.slug);
-
-  if (!post) {
-    return {
-      title: "Post Not Found",
-    };
-  }
-
-  return {
-    title: `${post.title} | Mohit Shrivastava`,
-    description: post.description,
-    keywords: post.tags || [],
-    openGraph: {
-      title: post.title,
-      description: post.description,
-      type: "article",
-      publishedTime: post.date,
-      authors: [post.author || "Mohit Shrivastava"],
-      images: post.image ? [{ url: post.image }] : [],
-    },
-  };
 }
 
 export default async function BlogPostPage({ params }) {
